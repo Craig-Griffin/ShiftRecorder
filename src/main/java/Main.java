19fec821d8d8997a769a12f1542fc5d1a1423355
Craig Griffin <craig.griffin350@hotmail.co.uk>
2019-03-27 20:01:30 +0000

    mvn configuration fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        ShiftModel shift = new ShiftModel("27-Mar-2019 19:00:00","27-Mar-2019 21:00:00");
+        ShiftModel shift = new ShiftModel("27-Mar-2019 19:00:00", "27-Mar-2019 21:00:00");
         ShiftsModel allShifts = new ShiftsModel();
 
 
         allShifts.addShift(shift);
-
         System.out.println(allShifts.getAllShifts().get(0).getDate());
 
 
